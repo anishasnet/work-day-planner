c33c4434a8975f894bd2d87b7eaabdc03e8a4ea2
anishasnet <anishasnet@gmail.com>
2020-04-12 16:44:38 -0700

    color coding based on time added

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,28 @@ currentDateText = moment().format("dddd, MMMM Do");
 $("#currentDay").html(currentDateText);
 
 
+let hours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+let hour;
+const dueDateCheck = () => {
+currentHour = moment().format("H")
+let k = 0;
+for (k  = 0; k < 9; k++) {
+
+if (currentHour > hours[k]) {
+$(".time-box").eq(k).removeClass("bg-success");
+$(".time-box").eq(k).addClass("bg-secondary");
+} else if (currentHour == hours[k]) {
+$(".time-box").eq(k).removeClass("bg-success");
+$(".time-box").eq(k).addClass("bg-primary");
+}
+}
+}
+
+dueDateCheck()
+
+setInterval(dueDateCheck, 5000)
+
+
 
 $(".col-8").on("click", ".list-group-item", function() {
 let textInput = $("<textarea rows = '1'>")
